0e12262a3d64abe335753774c3d61165dcbe5f90
Luram Archanjo <luram.archanjo@sensedia.com>
2020-03-25 20:22:11 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Overview
+
+This is a repository represents the Globo Television Advertisement.
+
+## Schema
+
+All the schemas are found in `/apache-avro-producer/src/main/avro`
+
+## Set up
+
+Start the kafka and kafka schema registry, using the command below:
+
+`docker-compose up`
+
+# References
+
+[Guide to Spring Cloud Stream with Kafka, Apache Avro and Confluent Schema Registry](https://www.baeldung.com/spring-cloud-stream-kafka-avro-confluent)
---
+++ src/main/kotlin/com/example/globo/AdvertisementService.kt
@@ -0,0 +1,27 @@
+package com.example.globo
+
+import com.example.advertisement.Advertisement
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.slf4j.LoggerFactory
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.stereotype.Service
+
+@Service
+class AdvertisementService() {
+
+    private final val log = LoggerFactory.getLogger(this::class.java)
+
+    private var advertisementKey: String? = null
+
+    @KafkaListener(topics = ["\${spring.kafka.template.default-topic}"])
+    fun consumer(record: ConsumerRecord<String, Advertisement>) {
+        if (advertisementKey == null) {
+            this.advertisementKey = record.key();
+        }
+
+        if (this.advertisementKey == record.key()) {
+            log.info("Consuming Key=[$advertisementKey] Message=[${record.value()}]")
+        }
+    }
+
+}
